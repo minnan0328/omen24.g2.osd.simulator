49aec302c37df264288ac3bd2571e70e88f42ffa
minnan0328 <minnan0328@gmail.com>
2025-09-23 20:23:29 +0800

    【調整】menu 垂直預設值

--- src/models/class/menu/_menu-position-nodes.ts
+++ src/models/class/menu/_menu-position-nodes.ts
@@ -9,8 +9,8 @@ const BackNodesEnum = new BackNodes();
 
 export default class MenuPositionNodes extends DefaultNodes implements Nodes {
     key = "MenuPosition";
-    selected = "H=98, V=2";
-    result = "H=98, V=2";
+    selected = "H=98, V=11";
+    result = "H=98, V=11";
     displayValue = true;
     size = 3;
     mode = ModeType.button;
@@ -57,8 +57,8 @@ export default class MenuPositionNodes extends DefaultNodes implements Nodes {
             // Range (0~100)
             ...JSON.parse(JSON.stringify(DefaultNodesEnum)),
             key: "Vertical",
-            selected: 2,
-            result: 2,
+            selected: 11,
+            result: 11,
             parents: this.key,
             mode: ModeType.horizontalRange,
             rangeMin: 0,
